5add846cde843cfd5f019793182470a75bff9065
aashishupreti <aashishupreti111@gmail.com>
2021-08-03 21:34:00 +0545

    commit

--- Agecalculate.js
+++ Agecalculate.js
@@ -1,14 +1,24 @@
 //Declaring values
 let name=["Aashish Upreti","Nitesh Upadhyaya", "Kiran Ghimire","Bibin Maharjan","Kushal Bajracharya", "Amresh Thakur"];
 let dates=["1996,11,31","1995,11,3","2001,11,11","1996,02,02","1998,10,26","1997,03,11"]
+
 //Age calculator
 function calculate_age(dob){
     let diff_month= Date.now()-dob.getTime();
     let age_date=new Date(diff_month);
     return Math.abs(age_date.getUTCFullYear()-1970);
 }
+
+//Day calculator
+function calculate_day(dob){
+    let diff_month= Date.now()-dob.getTime();
+    let month_date= new Date(diff_month);
+    return Math.abs(month_date.getUTCMonth()*30+month_date.getUTCDate());
+}
+
 //Displaying name and age
 for(i=0; i<name.length; i++){
     console.log("Full Name: " + name[i]);
-    console.log("Age is: "+ calculate_age(new Date(dates[i])));
+    console.log("Age is:"+ calculate_age(new Date(dates[i]))+"Year ");
+    console.log(calculate_day(new Date (dates[i]))+" Day");
 }
