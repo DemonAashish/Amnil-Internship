39cafe60b51d3c73e3b57193fcd24878ba52f626
aashishupreti <aashishupreti111@gmail.com>
2021-08-04 10:23:43 +0545

    Agecalculate

--- Agecalculate.js
+++ Agecalculate.js
@@ -1,24 +1,27 @@
 //Declaring values
-let name=["Aashish Upreti","Nitesh Upadhyaya", "Kiran Ghimire","Bibin Maharjan","Kushal Bajracharya", "Amresh Thakur"];
-let dates=["1996,11,31","1995,11,3","2001,11,11","1996,02,02","1998,10,26","1997,03,11"]
-
-//Age calculator
-function calculate_age(dob){
-    let diff_month= Date.now()-dob.getTime();
-    let age_date=new Date(diff_month);
-    return Math.abs(age_date.getUTCFullYear()-1970);
+const person = [    {firstName: 'Aayush', lastName: 'Shrestha', dob: '1986/09/23' },
+                    { firstName: 'Amresh', lastName: 'Thakur', dob: '1997/03/11' },
+                    { firstName: 'Bibin ', lastName: 'Maharjan', dob: '1996/02/02' },
+                    { firstName: 'Kiran ', lastName: 'Ghimire ', dob: '2001/11/11' },
+                    { firstName: 'Kushal', lastName: 'Bajracharya ', dob: '1998/10/26' },
+                    { firstName: 'Aashish', lastName: 'Upreti', dob: '1996/10/31' },
+                    { firstName: 'Nitesh', lastName: 'Upadhyaya', dob: '1997/11/02' }
+]; 
+//calculating year
+function today_date(dob){
+    let time_ms=Date.now()-dob.getTime();
+    let year=new Date(time_ms);
+    return Math.abs(year.getUTCFullYear()-1970);
 }
-
-//Day calculator
+//calculating day
 function calculate_day(dob){
     let diff_month= Date.now()-dob.getTime();
     let month_date= new Date(diff_month);
     return Math.abs(month_date.getUTCMonth()*30+month_date.getUTCDate());
 }
-
-//Displaying name and age
-for(i=0; i<name.length; i++){
-    console.log("Full Name: " + name[i]);
-    console.log("Age is:"+ calculate_age(new Date(dates[i]))+"Year ");
-    console.log(calculate_day(new Date (dates[i]))+" Day");
-}
+//display
+for(i=0; i<person.length; i++){
+    console.log(person[i].firstName +" "+person[i].lastName);
+    console.log("Your age is "+today_date(new Date(person[i].dob))+" year");
+    console.log(" "+ calculate_day(new Date(person[i].dob))+" Day" )
+}         
