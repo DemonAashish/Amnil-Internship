4c9c5ddcd624514d412ec8bb22a7e18fc37499f4
aashishupreti <aashishupreti111@gmail.com>
2021-08-10 10:33:56 +0545

    COM#2 : Correction

--- net-salary-calculator.js
+++ net-salary-calculator.js
@@ -3,8 +3,7 @@ const details =[
     {fullName:'John Doe', grossSalary:'9500'},
     {fullName:'Jane Doe', grossSalary:'16500'},
     {fullName:'Jim Doe', grossSalary:'24999'},
-    {fullName:'Josh Doe', grossSalary:'37000'},
-    {fullName:'Josh Doe', grossSalary:'32424'}
+    {fullName:'Josh Doe', grossSalary:'37000'}
 ];
 //Tax Slabs
 const taxSlabs = [
@@ -14,30 +13,28 @@ const taxSlabs = [
     { gt: 30000, lte: 9999999999, tax: 10},
 ];
 let netSalary=[];
-let gt=[];
-let lte=[];
-let tax=[];
-const grossSalary=details.grossSalary;
-for (j=0;j<taxSlabs.length;j++){
-    gt[j]=taxSlabs[j].gt;
-    lte[j]=taxSlabs[j].lte;
-    tax[j]=taxSlabs[j].tax; 
+netSalaryCalculator=(grossSalary)=>{
+    let remainderGrossSalary=grossSalary;
+    let totalTaxAmount=0;
+    taxSlabs.forEach(slab =>{
+        const range=slab.lte-slab.gt;
+        let taxableAmt=0;
+        let taxAmount=0;
+        if(remainderGrossSalary<=range){
+            taxableAmt=remainderGrossSalary;
+            remainderGrossSalary=0;
+        } 
+        else{
+            taxableAmt=range;
+            remainderGrossSalary=remainderGrossSalary-range;
+        }
+        taxAmount=taxableAmt*(slab.tax/100);
+        totalTaxAmount =taxAmount+totalTaxAmount;
+    })
+    return Math.ceil(totalTaxAmount);
 }
-for(i=0;i<details.length;i++){
-const grossSalary=details[i].grossSalary; 
+details.map(m=>{
+    console.log(`${m.fullName} ${m.grossSalary-netSalaryCalculator(m.grossSalary)}`)
+})
 
-        if (taxSlabs[0]){
-            netSalary=grossSalary-grossSalary*tax[0]/100;
-        }
-        else if(taxSlabs[1]){
-            netSalary=(lte[0]-gt[0])-(lte[0]-gt[0])*tax[0]/100-(grossSalary-lte)*tax/100+(grossSalary-lte);
-        }
-        else if(taxSlabs[2]){
-            netSalary=(lte[0]-gt[0])-(lte[0]-gt[0])*tax[0]/100+(lte[1]-gt[1])-(lte[1]-gt[1])*tax[1]/100+(grossSalary-lte)-(grossSalary-lte)*0.07;
-        }
-        else {
-            netSalary=(lte[0]-gt[0])-(lte[0]-gt[0])*tax[0]/100+(lte[1]-gt[1])-(lte[1]-gt[1])*tax[1]/100+(lte[2]-gt[2])-(lte[2]-gt[2])*tax[2]/100 +(grossSalary-lte)-(grossSalary-lte)*0.1;
-        }
-    
-        console.log("Name: "+details[i].fullName+" Net Salary: "+netSalary);
-}
+
